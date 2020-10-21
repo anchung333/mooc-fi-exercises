3bd10a6c6d73d0791fa74e9c4760d44c749b5fe0
Andy <xcessivepupil2@gmail.com>
2020-10-21 13:11:44 -0700

    Add exercises from Part 12.3

---
+++ part2/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class LotteryRow {
+
+    private ArrayList<Integer> numbers;
+
+    public LotteryRow() {
+        // Draw the numbers when the LotteryRow is created
+        this.randomizeNumbers();
+    }
+
+    public ArrayList<Integer> numbers() {
+        return this.numbers;
+    }
+
+    public void randomizeNumbers() {
+        // Initialize the list for numbers
+        this.numbers = new ArrayList<>();
+        Random rand = new Random();
+        // Implement the random number generation here
+        // the method containsNumber is probably useful
+        while (this.numbers.size() < 7) {
+            int randInt = rand.nextInt(40) + 1;
+            if (!this.containsNumber(randInt)) {
+                this.numbers.add(randInt);
+            }
+        }
+    }
+
+    public boolean containsNumber(int number) {
+        // Check here whether the number is among the drawn numbers
+        if (this.numbers.contains(number)) {
+            return true;
+        }
+        return false;
+    }
+}
+
